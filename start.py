02f8194bcebe6afdb9e653da61852f54654551a5
Edvard Pedersen <edvard.pedersen@uit.no>
2025-04-03 12:03:11 +0200

    update assignment text

--- start.py
+++ start.py
@@ -1,6 +1,5 @@
 import pygame
 
-
 class Movable:
   def __init__(self, rect):
     self.rect = rect
@@ -9,13 +8,30 @@ class Movable:
     color = (255, 0, 0)
     surface.fill(color, self.rect)
 
+class Circle(Movable):
+  def draw(self, surface):
+    color = (255, 0, 0)
+    pygame.draw.circle(surface, color, self.rect.center, self.rect.w / 2)
+
+class Box(Movable):
+  def draw(self, surface):
+    color = (0, 255, 0)
+    pygame.draw.rect(surface, color, self.rect)
+
+class Triangle(Movable):
+  def draw(self, surface):
+    color = (0, 0, 255)
+    pygame.draw.polygon(surface, color, [self.rect.midtop, self.rect.bottomright, self.rect.bottomleft])
+
 class Game:
   def __init__(self):
     pygame.init()
     self.screen = pygame.display.set_mode((800,800))
     self.movables = []
     for x in range(100, 600, 100):
-      self.movables.append(Movable(pygame.Rect(x, 100, 50, 50)))
+      self.movables.append(Circle(pygame.Rect(x, 100, 50, 50)))
+      self.movables.append(Box(pygame.Rect(x, 300, 50, 50)))
+      self.movables.append(Triangle(pygame.Rect(x, 500, 50, 50)))
 
   def mainloop(self):
     for event in pygame.event.get():
